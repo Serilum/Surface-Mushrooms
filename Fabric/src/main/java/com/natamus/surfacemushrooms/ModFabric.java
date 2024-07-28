c47c8cd0773e3af9358c4d7924890c46bc0bb2fa
Rick South <rick@natamus.com>
2024-07-28 07:12:40 +0200

    (v3.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/surfacemushrooms/ModFabric.java
+++ Fabric/src/main/java/com/natamus/surfacemushrooms/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.surfacemushrooms;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.collective.fabric.callbacks.CollectiveBlockEvents;
 import com.natamus.surfacemushrooms.events.MushroomBlockEvent;
 import com.natamus.surfacemushrooms.util.Reference;
@@ -15,6 +16,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
