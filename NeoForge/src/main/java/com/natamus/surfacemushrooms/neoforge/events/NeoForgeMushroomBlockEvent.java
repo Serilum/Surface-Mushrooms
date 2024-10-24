e570e0e72438f558e9067225e47b8ef3f483ef3c
Rick South <rick@natamus.com>
2024-10-24 17:58:35 +0200

    Surface Mushrooms source code for 1.21.2.

---
+++ NeoForge/src/main/java/com/natamus/surfacemushrooms/neoforge/events/NeoForgeMushroomBlockEvent.java
@@ -0,0 +1,16 @@
+package com.natamus.surfacemushrooms.neoforge.events;
+
+import com.natamus.surfacemushrooms.events.MushroomBlockEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeMushroomBlockEvent {
+	@SubscribeEvent
+	public static void onMushroomPlace(PlayerInteractEvent.RightClickBlock e) {
+		if (!MushroomBlockEvent.onMushroomPlace(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec())) {
+			e.setCanceled(true);
+		}
+	}
+}
