35091ab85e3fe8d142045d6fe501ecefca5dbede
Rick South <rick@natamus.com>
2025-02-12 23:11:04 +0100

    (v3.6) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/surfacemushrooms/ModForge.java
+++ Forge/src/main/java/com/natamus/surfacemushrooms/ModForge.java
@@ -28,7 +28,7 @@ public class ModForge {
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-        MinecraftForge.EVENT_BUS.register(new ForgeMushroomBlockEvent());
+        MinecraftForge.EVENT_BUS.register(ForgeMushroomBlockEvent.class);
 	}
 
 	private static void setGlobalConstants() {
