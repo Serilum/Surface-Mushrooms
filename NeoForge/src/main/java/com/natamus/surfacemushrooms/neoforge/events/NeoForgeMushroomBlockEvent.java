e7a6b56831fa86c88f387d7cc729a71c2038f362
Rick South <rick@natamus.com>
2025-02-12 23:10:59 +0100

    (v3.6) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/surfacemushrooms/neoforge/events/NeoForgeMushroomBlockEvent.java
+++ NeoForge/src/main/java/com/natamus/surfacemushrooms/neoforge/events/NeoForgeMushroomBlockEvent.java
@@ -3,9 +3,7 @@ package com.natamus.surfacemushrooms.neoforge.events;
 import com.natamus.surfacemushrooms.events.MushroomBlockEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeMushroomBlockEvent {
 	@SubscribeEvent
 	public static void onMushroomPlace(PlayerInteractEvent.RightClickBlock e) {
